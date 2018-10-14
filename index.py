fda8ce9bb975a5c19c2ea63a3d5d8192eefe9982
Васин Юрий <george.a.wise@gmail.com>
2018-10-14 20:17:05 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+'''
 def app(environ, start_response):
 	data = b"Hello, World!\n"
 	start_response("200 OK", [
@@ -5,3 +6,13 @@ def app(environ, start_response):
 		("Content-Length", str(len(data)))
 	])
 	return iter([data])
+'''
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def source():
+ html = 'Hello World!'
+ return html
