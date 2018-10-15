d8c077f0af0081a6ba39f8ed87ec188db04504a3
Васин Юрий <george.a.wise@gmail.com>
2018-10-15 16:17:24 +0300

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+#import urlparse
+from urllib.parse import urlparse
+import psycopg2
+import os
+
+url = urlparse(os.environ['DATABASE_URL'])
+dbname = url.path[1:]
+user = url.username
+password = url.password
+host = url.hostname
+port = url.port
+
+con = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
+
+app = Flask(__name__)
+
+from app import views
